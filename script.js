faff86054a493f5042a225843c0d18f34e5d8d8f
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-13 09:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+// Get the container element
+const container = document.querySelector('.container');
+
+// Function to create a square element
+function createSquare() {
+    const square = document.createElement('div');
+    square.className = 'square';
+    return square;
+}
+
+// Function to add event listener to the square element
+function addEventListenerToSquare(square) {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
+    });
+
+    square.addEventListener('mouseout', () => {
+        square.style.backgroundColor = '#fff';
+    });
+}
+
+// Function to create and add squares to the container
+function createAndAddSquares() {
+    for (let i = 0; i < 800; i++) {
+        const square = createSquare();
+        addEventListenerToSquare(square);
+        container.appendChild(square);
+    }
+}
+
+// Create and add squares to the container
+createAndAddSquares();
